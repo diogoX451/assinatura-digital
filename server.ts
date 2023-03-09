1f0b101e7c7c33a4678de7f3c40527a3ddc859bc
diogoX451 <diogo.g07@icloud.com>
2023-03-09 08:41:48 -0300

    atualização

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
-import router from "./Router/router";
+import router from "./src/Router/router";
 const app = express();
 const port = 8182;
 app.use(express.json());
